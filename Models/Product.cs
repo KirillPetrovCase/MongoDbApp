64d3016922d3cf077c89cc16e77ed1cc4e8022b4
KirillPetrovCase <kirill.petrov.work.email@gmail.com>
2021-06-24 15:51:22 +0300

    Added ProductService

---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MongoDbApp.Models
+{
+    public class Product
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [Display(Name = "Модель")]
+        public string Name { get; set; }
+        [Display(Name = "Производитель")]
+        public string Company { get; set; }
+        [Display(Name = "Цена")]
+        public int Price { get; set; }
+        public string ImageId { get; set; }
+        public bool HasImage() => String.IsNullOrWhiteSpace(ImageId) is false;
+    }
+}
---
+++ Models/ProductService.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDbApp.Models
+{
+    public class ProductService
+    {
+        private readonly IGridFSBucket _gridFS;
+        private readonly IMongoCollection<Product> _products;
+
+        public ProductService(IConfiguration configuration)
+        {
+            string mongoConnectionString = configuration.GetConnectionString("MongoDbConnectionString");
+            MongoUrlBuilder connection = new(mongoConnectionString);
+            MongoClient mongoClient = new(mongoConnectionString);
+
+            IMongoDatabase mongoDatabase = mongoClient.GetDatabase(connection.DatabaseName);
+
+            _gridFS = new GridFSBucket(mongoDatabase);
+            _products = mongoDatabase.GetCollection<Product>("Products");
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts(int? minPrice, int? maxPrice, string name)
+        {
+            FilterDefinitionBuilder<Product> builder = new();
+            FilterDefinition<Product> filter = builder.Empty;
+
+            if (string.IsNullOrWhiteSpace(name) is false)
+            {
+                filter &= builder.Regex("Name", new BsonRegularExpression(name));
+            }
+            if (minPrice.HasValue is true)  // фильтр по минимальной цене
+            {
+                filter &= builder.Gte("Price", minPrice.Value);
+            }
+            if (maxPrice.HasValue is true)  // фильтр по максимальной цене
+            {
+                filter &= builder.Lte("Price", maxPrice.Value);
+            }
+
+            return await _products.Find(filter).ToListAsync();
+            //return await _products.Find(product => product.Name == name).ToListAsync();
+        }
+
+        public async Task<Product> GetProduct(string id) => await _products.Find(new BsonDocument("_id", new ObjectId(id))).FirstOrDefaultAsync();
+
+        public async Task Create(Product product) => await _products.InsertOneAsync(product);
+
+        public async Task Update(Product product) => await _products.ReplaceOneAsync(new BsonDocument("_id", new ObjectId(product.Id)), product);
+
+        public async Task Remove(string id) => await _products.DeleteOneAsync(new BsonDocument("_id", new ObjectId(id)));
+
+        public async Task<byte[]> GetImage(string id) => await _gridFS.DownloadAsBytesAsync(new ObjectId(id));
+
+        public async Task StoreImage(string id, Stream imageStream, string imageName)
+        {
+            Product p = await GetProduct(id);
+
+            //If product already has image delete it
+            if (p.HasImage() is true) await _gridFS.DeleteAsync(new ObjectId(p.ImageId));
+
+            ObjectId imageId = await _gridFS.UploadFromStreamAsync(imageName, imageStream);
+
+            p.ImageId = imageId.ToString();
+
+            var filter = Builders<Product>.Filter.Eq("_id", new ObjectId(p.Id));
+            var update = Builders<Product>.Update.Set("ImageId", p.ImageId);
+
+            await _products.UpdateOneAsync(filter, update);
+        }
+    }
+}
